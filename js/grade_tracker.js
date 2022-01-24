9447c90305ec1a7615551fc9eddb1b7dbe1df79d
deraphel <75043986+jho1211@users.noreply.github.com>
2022-01-23 18:02:03 -0800

    Grade

--- js/grade_tracker.js
+++ js/grade_tracker.js
@@ -1,19 +1,21 @@
-gradeData = {}
+var gradeData = {};
+var totalRows = 0;
 
 function newRow(){
 	var aList = document.getElementById("assignmentList");
 	var newAssign = document.createElement("DIV");
 	newAssign.className = "row"
 	newAssign.innerHTML = `<div class="col-4 text-light mb-2">
-              <input type="text" class="form-control" id="courseName" placeholder="Assignment Name" required>
+              <input type="text" class="form-control" id="aname-${totalRows}" placeholder="Assignment Name" required>
             </div>
             <div class="col-4 text-light mb-2">
-              <input type="text" class="form-control" id="courseName" placeholder="0%" required>
+              <input type="text" class="form-control" id="weight-${totalRows}" placeholder="0%" required>
             </div>
             <div class="col-4 text-light mb-2">
-              <input type="text" class="form-control" id="courseName" placeholder="0%" required>
+              <input type="text" class="form-control" id="grade-${totalRows}" placeholder="0%" required>
             </div>`
     aList.appendChild(newAssign);
+    totalRows += 1;
 }
 
 function deleteRow(){
@@ -30,9 +32,48 @@ function deleteRow(){
 
 function submitGrades(){
 	console.log("Submitted grades");
-	cName = document.getElementById("cName").value;
-	var aList = document.getElementById("assignmentList");
-	console.log(aList.childNodes);
+	cName = document.getElementById("courseName").value;
 
-	gradeData[cName] = [];
+	var newArr = []
+	var numAssigns = document.getElementById("assignmentList").getElementsByTagName("input").length / 3;
+	var curGrade = 0;
+	var totalWeight = 0;
+	var totalGrade = 0;
+
+	for (var i = 0; i < numAssigns; i++){
+		let aname = document.getElementById("aname-" + i).value;
+		let weight = document.getElementById("weight-" + i).value;
+		let grade = document.getElementById("grade-" + i).value;
+
+		if (weight.endsWith("%")){
+			weight = parseFloat(weight.slice(0, -1)) / 100
+		}
+		else{
+			weight = parseFloat(weight)
+		}
+
+		if (grade.endsWith("%")){
+			grade = parseFloat(grade.slice(0, -1))
+		}
+		else{
+			grade = parseFloat(grade) * 100
+		}
+
+		if (isNaN(weight) || isNaN(grade)){
+			alert(`The weight or grade for assignment ${i+1} is incorrectly formatted, please check it again.`);
+			return;
+		}
+
+		curGrade += weight * grade
+		totalWeight += weight
+		totalGrade += weight * 100
+
+		newArr.push([aname, weight, grade]);
+	}
+
+	let actualGrade = (curGrade / totalGrade * 100).toFixed(2);
+
+	gradeData[cName] = newArr;
+	alert("Your current grade in the course is " + actualGrade + "%")
+	// Need to add a new element that shows the grade and assignments for each course saved
 }
